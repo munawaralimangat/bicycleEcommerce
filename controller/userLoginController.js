930e38021967f683e32a35f882844958746183ea
Munawar Ali M <munawaralimangat@gmail.com>
2023-10-30 15:59:30 +0530

    changed controller

--- controller/userLoginController.js
+++ controller/userLoginController.js
@@ -5,168 +5,238 @@ const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 
 
-const userLandingView = async (req, res) => {
-    res.render('user/landing', {
-        regLog: "Log In",
-        formurl: "login"
-    })
+module.exports.landingView =(req,res)=>{
+    res.render('user/landing',{
+                regLog: "Log In",
+                formurl: "login"
+            })
 }
 
-const userRegView = async (req, res) => {
+
+
+module.exports.userRegView =(req,res)=>{
     res.render('user/userReg', {
-        regLog: "Log In",
-        formurl: "login",
-        errors: ""
-    })
+                regLog: "Log In",
+                formurl: "login",
+                errors: ""
+            })
 }
 
-const userRegPost = async (req, res) => {
-    console.log(req.body)
-    try {
-        const { user_firstName, user_secondName, user_email, user_number, user_password, confirm_password } = req.body;
-        if (!(user_firstName && user_secondName && user_email && user_number && user_password && confirm_password)) {
-            let error = 'all fields are required'
-            res.status(400).render('user/userReg', {
-                regLog: "Log In",
-                formurl: "login",
-                errors: ""
-            })
-            return false
-        }
-        // Check if the password and confirm password match
-        if (user_password !== confirm_password) {
-            let error = 'confirm password'
-            res.status(400).render('user/userReg', {
-                regLog: "Log In",
-                formurl: "login",
-                errors: ""
-            })
-            return false
-        }
-        //check if user already exists
-        const existingUser = await User.findOne({ user_email })
-        if (existingUser) {
-            let error = 'user already exists with that email'
-            res.status(400).render('user/userReg', {
-                regLog: "Log In",
-                formurl: "login",
-                errors: ""
-            })
-            return false
-        }
-        //encrypt password
-        const encryptedPassword = await bcryptjs.hash(user_password, 10)
-        //save user in db
-        const user = await User.create({
-            user_firstName,
-            user_secondName,
-            user_email,
-            user_number,
-            user_password: encryptedPassword
-        })
-        //generate a token for user
-        const token = jwt.sign(
-            { id: user._id, user_email },
-            'shhhh',
-            {
-                expiresIn: '1h'
-            }
-        )
-        user.token = token
-        user.user_password = undefined
-        console.log(token)
+module.exports.userRegPost =(req,res)=>{
+    res.render('user/userReg')
+}
 
-        res.status(201).render('user/userLogin', {
-            formurl: "register",
-            regLog: "Register",
-            errors: ""
-        })
-        console.log("end")
-    } catch (error) {
-        console.log(error)
-    }
-};
-
-
-
-const userLoginView = async (req, res) => {
+module.exports.userLoginView =(req,res)=>{
     res.render('user/userLogin', {
-        regLog: "Register",
-        formurl: "register",
-        errors: ""
-    })
+                regLog: "Register",
+                formurl: "register",
+                errors: ""
+            })
+}
+
+module.exports.userLoginPost =(req,res)=>{
+    res.render('user/userReg')
+}
+
+module.exports.userHome =(req,res)=>{
+    res.render('user/userHome')
 }
 
 
-const userLoginPost = async (req, res) => {
-    try {
-        // Get user_email and user_password from the request body
-        const { user_email, user_password } = req.body;
-
-        // Validate if both user_email and user_password are provided
-        if (!(user_email && user_password)) {
-            console.log(user_email, user_password);
-            return res.status(400).render('user/userLogin', {
-                regLog: "Register",
-                formurl: "register",
-                errors: "Email and password are required"
-            });
-        }
-
-        // Find the user in the database
-        const user = await User.findOne({ user_email });
-
-        // If the user is not found, send an error response
-        if (!user) {
-            return res.status(401).render('user/userLogin', {
-                regLog: "Register",
-                formurl: "register",
-                errors: "Invalid email or password"
-            });
-        }
-
-        // Check if the provided password matches the hashed password in the database
-        const passwordMatches = await bcryptjs.compare(user_password, user.user_password);
-
-        if (passwordMatches) {
-            // Generate a token
-            const token = jwt.sign(
-                { id: user._id },
-                'shhhh',
-                {
-                    expiresIn: '1h'
-                }
-            );
-
-            // Set the token in a cookie
-            const options = {
-                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-                httpOnly: true
-            };
-            res.cookie('token', token, options);
-
-            // Render the "user/userLogin" page with the token
-            return res.status(200).render('user/userHome', {
-                regLog: "Register",
-                formurl: "register",
-                token: token
-            });
-        } else {
-            // If the password doesn't match, send an error response
-            return res.status(401).render('user/userLogin', {
-                regLog: "Register",
-                formurl: "register",
-                errors: "Invalid email or password"
-            });
-        }
-    } catch (error) {
-        console.log(error);
-        // Handle any other errors gracefully
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-};
 
 
 
 
-module.exports = { userLandingView, userLoginView, userLoginPost, userRegView, userRegPost }//login
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const userLandingView = async (req, res) => {
+//     res.render('user/landing', {
+//         regLog: "Log In",
+//         formurl: "login"
+//     })
+// }
+
+// const userRegView = async (req, res) => {
+//     res.render('user/userReg', {
+//         regLog: "Log In",
+//         formurl: "login",
+//         errors: ""
+//     })
+// }
+
+// const userRegPost = async (req, res) => {
+//     console.log(req.body)
+//     try {
+//         const { user_firstName, user_secondName, user_email, user_number, user_password, confirm_password } = req.body;
+//         if (!(user_firstName && user_secondName && user_email && user_number && user_password && confirm_password)) {
+//             let error = 'all fields are required'
+//             res.status(400).render('user/userReg', {
+//                 regLog: "Log In",
+//                 formurl: "login",
+//                 errors: error
+//             })
+//             return false
+//         }
+//         // Check if the password and confirm password match
+//         if (user_password !== confirm_password) {
+//             let error = 'confirm password'
+//             res.status(400).render('user/userReg', {
+//                 regLog: "Log In",
+//                 formurl: "login",
+//                 errors: error
+//             })
+//             return false
+//         }
+//         //check if user already exists
+//         const existingUser = await User.findOne({ user_email })
+//         if (existingUser) {
+//             let error = 'user already exists with that email'
+//             res.status(400).render('user/userReg', {
+//                 regLog: "Log In",
+//                 formurl: "login",
+//                 errors: error
+//             })
+//             return false
+//         }
+//         //encrypt password
+//         const encryptedPassword = await bcryptjs.hash(user_password, 10)
+//         //save user in db
+//         const user = await User.create({
+//             user_firstName,
+//             user_secondName,
+//             user_email,
+//             user_number,
+//             user_password: encryptedPassword
+//         })
+//         //generate a token for user
+//         const token = jwt.sign(
+//             { id: user._id, user_email },
+//             'shhhh',
+//             {
+//                 expiresIn: '1h'
+//             }
+//         )
+//         user.token = token
+//         user.user_password = undefined
+//         console.log(token)
+
+//         res.status(201).render('user/userLogin', {
+//             formurl: "register",
+//             regLog: "Register",
+//             errors: ""
+//         })
+//         console.log("end")
+//     } catch (error) {
+//         console.log(error)
+//     }
+// };
+
+
+
+// const userLoginView = async (req, res) => {
+//     res.render('user/userLogin', {
+//         regLog: "Register",
+//         formurl: "register",
+//         errors: ""
+//     })
+// }
+
+
+// const userLoginPost = async (req, res) => {
+//     try {
+//         // Get user_email and user_password from the request body
+//         const { user_email, user_password } = req.body;
+
+//         // Validate if both user_email and user_password are provided
+//         if (!(user_email && user_password)) {
+//             console.log(user_email, user_password);
+//             return res.status(400).render('user/userLogin', {
+//                 regLog: "Register",
+//                 formurl: "register",
+//                 errors: "Email and password are required"
+//             });
+//         }
+
+//         // Find the user in the database
+//         const user = await User.findOne({ user_email });
+
+//         // If the user is not found, send an error response
+//         if (!user) {
+//             return res.status(401).render('user/userLogin', {
+//                 regLog: "Register",
+//                 formurl: "register",
+//                 errors: "Invalid email or password"
+//             });
+//         }
+
+//         // Check if the provided password matches the hashed password in the database
+//         const passwordMatches = await bcryptjs.compare(user_password, user.user_password);
+
+//         if (passwordMatches) {
+//             // Generate a token
+//             const token = jwt.sign(
+//                 { id: user._id },
+//                 'shhhh',
+//                 {
+//                     expiresIn: '1h'
+//                 }
+//             );
+
+//             // Set the token in a cookie
+//             const options = {
+//                 expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+//                 httpOnly: true
+//             };
+//             res.cookie('token', token, options);
+
+//             // Render the "user/userLogin" page with the token
+//             return res.status(200).render('user/userHome', {
+//                 regLog: "Register",
+//                 formurl: "register",
+//                 token: token
+//             });
+//         } else {
+//             // If the password doesn't match, send an error response
+//             return res.status(401).render('user/userLogin', {
+//                 regLog: "Register",
+//                 formurl: "register",
+//                 errors: "Invalid email or password"
+//             });
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         // Handle any other errors gracefully
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// };
+
+// const userHomeView = async (req,res) =>{
+//     res.render('user/userHome')
+// }
+
+
+
+// module.exports = { userLandingView, userLoginView, userLoginPost, userRegView, userRegPost,userHomeView}//login
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,51 @@
 const express = require('express')
+const userController = require('../controller/userLoginController')
 const router = express.Router()
 
-const {userLandingView,userLoginView,userLoginPost,userRegView, userRegPost} = require('../controller/userLoginController')
 
-//landing page
-router.get('/landing',userLandingView)
 
-//user login view
-router.get('/landing/login',userLoginView)
+router.get('/landing',userController.landingView)
 
-//user registration
-router.get('/landing/register',userRegView)
-router.post('/landing/register',userRegPost)
 
-//user login
-router.post('/landing/login',userLoginPost)
+router.get('/landing/register',userController.userRegView)
+router.post('/landing/register',userController.userRegPost)
+router.get('/landing/login',userController.userLoginView)
+router.post('/landing/login',userController.userLoginPost)
+
+
+router.get('/landing/userHome',userController.landingView)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {userLandingView,userLoginView,userLoginPost,userRegView,userRegPost,userHomeView} = require('../controller/userLoginController')
+
+// //landing page
+// router.get('/landing',userLandingView)
+
+// //user login view
+// router.get('/landing/login',userLoginView)
+
+// //user registration
+// router.get('/landing/register',userRegView)
+// router.post('/landing/register',userRegPost)
+
+// //user login
+// router.post('/landing/login',userLoginPost)
+
+// //user landing View
+// router.get('/landing/userLanding',userHomeView)
 
 
 
