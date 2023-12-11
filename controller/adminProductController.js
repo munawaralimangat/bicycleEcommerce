3e471c969867b2a8f52293e7b6afad14c6fc87fb
Munawar Ali M <munawaralimangat@gmail.com>
2023-12-11 20:53:34 +0530

    updated

--- controller/adminProductController.js
+++ controller/adminProductController.js
@@ -24,7 +24,10 @@ module.exports.getProduct = async (req,res)=>{
     try {
         const productId = req.params.productId;
     
-        const product = await Product.findById(productId).populate('category_name')
+        const product = await Product.findById(productId)
+        .populate('category_name')
+        .populate('product_size')
+        .populate('product_colour')
         console.log(product)
     
         if (!product) {
@@ -66,8 +69,8 @@ module.exports.createProduct = async (req,res)=>{
         const size = existingSize || new Size({size_name:productSize});
         const savedSize = await size.save()
 
-        const existingColor = await Color.findOne({color_name:productSize});
-        const color = existingColor || new Color({color_name:productSize});
+        const existingColor = await Color.findOne({color_name:productColour});
+        const color = existingColor || new Color({color_name:productColour});
         const savedColor = await color.save()
 
         
@@ -86,7 +89,7 @@ module.exports.createProduct = async (req,res)=>{
     
         const savedProduct = await newProduct.save();
     
-        res.status(201).json({savedProduct});
+        res.status(201).json({message:savedProduct});
       } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -106,8 +109,8 @@ module.exports.updateProduct = async (req,res)=>{
         discountPrice,
     } = req.body;
   
-    const frontImage = req.files.frontImage[0];
-    const additionalImages = req.files.additionalImages;
+    const frontImage = req.files.frontImage ? req.files.frontImage[0] : undefined;
+    const additionalImages = req.files.additionalImages || [];
     console.log("front image: ",frontImage)
     console.log("images",additionalImages)
 
@@ -118,26 +121,56 @@ module.exports.updateProduct = async (req,res)=>{
         existingCategory = await existingCategory.save();
     }
 
-    // Assuming you have a Product model with the appropriate schema
-    const updatedProduct = await Product.findByIdAndUpdate(
-        productId,
-        {
-          product_name:productName,
-          product_price:productPrice,
-          category_name:existingCategory._id,
-          product_qty:productQty,
-          product_size:productSize,
-          product_colour:productColour,
-          discount_price:discountPrice,
-          front_image:frontImage,
-          product_images: additionalImages,
-        },
-        { new: true }
-    );
+        const existingSize = await Size.findOne({size_name:productSize});
+        const size = existingSize || new Size({size_name:productSize});
+        const savedSize = await size.save()
 
-    if (!updatedProduct) {
-        return res.status(404).json({ error: 'Product not found' });
-    }
+        const existingColor = await Color.findOne({color_name:productColour});
+        const color = existingColor || new Color({color_name:productColour});
+        const savedColor = await color.save()
+
+        const existingProduct = await Product.findById(productId);
+        if(!existingProduct){
+          return res.status(404).json({error:"product not found"})
+        }
+
+        existingProduct.product_name = productName;
+        existingProduct.product_price = productPrice;
+        existingProduct.category_name = existingCategory._id;
+        existingProduct.product_qty = productQty;
+        existingProduct.product_size = savedSize._id;
+        existingProduct.product_colour = savedColor._id;
+        existingProduct.discount_price = discountPrice;
+
+        if (frontImage) {
+          existingProduct.front_image = frontImage;
+        }
+        if (additionalImages.length > 0) {
+          existingProduct.product_images = additionalImages;
+        }
+        const updatedProduct = await existingProduct.save();
+
+    // Assuming you have a Product model with the appropriate schema
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    // const updatedProduct = await Product.findByIdAndUpdate(
+    //     productId,
+    //     {
+    //       product_name:productName,
+    //       product_price:productPrice,
+    //       category_name:existingCategory._id,
+    //       product_qty:productQty,
+    //       product_size:savedSize._id,
+    //       product_colour:savedColor._id,
+    //       discount_price:discountPrice,
+    //       front_image:frontImage,
+    //       product_images: additionalImages,
+    //     },
+    //     { new: true }
+    // );
+
+    // if (!updatedProduct) {
+    //     return res.status(404).json({ error: 'Product not found' });
+    // }
 
     res.json({
         message: 'Product updated successfully',
