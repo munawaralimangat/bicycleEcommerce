3d9756eb4ab07280f34ecf9e25a7cc6fb4bad0cd
Munawar Ali M <munawaralimangat@gmail.com>
2023-12-13 09:19:39 +0530

    updated

--- controller/userProductController.js
+++ controller/userProductController.js
@@ -3,11 +3,19 @@ const Product = require('../model/schema/productSchema');
 module.exports.viewProduct = async (req,res)=>{
     try {
         const productId = req.params.productId
-        const product = await Product.findById(productId).populate('category_name')
+        const product = await Product.findById(productId)
+        .populate('category_name')
+        .populate({
+            path: 'variations.color',
+            model: 'Color',
+        })
+        .populate({
+            path: 'variations.size',
+            model: 'Size',
+        });
         res.render('user/product',{product})
     } catch (error) {
         console.error('Error fetching product details:', error);
         res.status(500).send('Internal Server Error');
     }
-    
 }
