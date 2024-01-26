b6f80b2455d06ce838e62c4d7b125c2a4d19f30c
Munawar Ali M <munawaralimangat@gmail.com>
2024-01-26 22:57:58 +0530

    gitignore

---
+++ model/schema/otpSchema.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const otpSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+    },
+    otp:{
+        type:String,
+        required:true,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now(),
+        expires:60*5
+    }
+});
+
+//function for send a mail
+
+async function sendVerificationEmail(){
+    try {
+        const mailresponse = await mailSender(
+            email,
+            "Verification email",
+            `<h1> Please confirm your OTP</h1>
+            <h1> here is your ${otp}</h1>`
+        );
+        console.log("Email sent: ",mailresponse)
+    } catch (error) {
+        console.error("Error occured sending email",error);
+        throw error
+    }   
+}
+otpSchema.pre("save", async function (next) {
+    console.log("New document saved to the database");
+    if (this.isNew) {
+      await sendVerificationEmail(this.email, this.otp);
+    }
+    next();
+  });
+
+  module.exports = mongoose.model("OTP", otpSchema);
---
+++ utils/mailSender.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer')
+
+const mailsender = async (email,title,body)=>{
+    try {
+        let transporter = nodemailer.createTransport({
+            
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
